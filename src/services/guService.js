047fc111ef1d90f98df1a69e9e304876d7b7b6c6
emmanueledurante <emmanuele.durante@martechrevolution.com>
2024-01-24 08:32:42 -0300

    dwa13

--- src/services/guService.js
+++ src/services/guService.js
@@ -48,7 +48,7 @@ const getDataFromFile = async (filePath) => {
       try {
         let result = await processRows(
           parsedRows,
-          transformDateTimeArray,
+          transformDateTimeArray,≤
           calculateTotalHours
         );
         return result;
