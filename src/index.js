1cefe2856efba70f9f53ac5449d4c520cc7cd8a8
emmanueledurante <emmanuele.durante@martechrevolution.com>
2024-01-23 21:44:54 -0300

    dwa2

--- src/index.js
+++ src/index.js
@@ -11,9 +11,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(
-  process.env.GU_BASE_API_PATH + "/" + process.env.GU_API_SECRET + "/",
-  v1GuRoutes
-);
+app.use("/api/v1/gu-calculator", v1GuRoutes);
 
 app.listen(PORT, () => console.log(`gu-calculator is running on port ${PORT}`));
